df799b7429764f9fa347a40a8ecddd7a6bde0f71
nic-cab-sor <nicole.cabaya.soriano@gmail.com>
2026-05-05 23:57:12 +0100

    Modified project structure, created Header component, tested it in main screen.

---
+++ app/src/main/java/com/nicole/expensestracker/ui/components/Header.kt
@@ -0,0 +1,15 @@
+package com.nicole.expensestracker.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.TextUnit
+
+@Composable
+fun Header(title: String, fontSize: TextUnit, modifier: Modifier) {
+    Text(
+        text = title,
+        fontSize = fontSize,
+        modifier = modifier
+    )
+}
--- app/src/main/java/com/nicole/expensestracker/ui/screens/MainActivity.kt
+++ app/src/main/java/com/nicole/expensestracker/ui/screens/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nicole.expensestracker
+package com.nicole.expensestracker.ui.screens
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.nicole.expensestracker.ui.components.Header
 import com.nicole.expensestracker.ui.theme.ExpensesTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +19,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExpensesTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Header("This is the title", 26.sp, Modifier.padding(innerPadding))
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExpensesTrackerTheme {
-        Greeting("Android")
-    }
-}
+// Examples
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ExpensesTrackerTheme {
+//        Greeting("Android")
+//    }
+//}
